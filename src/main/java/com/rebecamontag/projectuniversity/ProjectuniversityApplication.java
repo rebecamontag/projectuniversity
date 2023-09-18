880c7124f054df32c15dcace0d2e10c5ad39b5f9
Rebeca Montagnana <montagrebeca@gmail.com>
2023-09-18 09:12:27 -0300

    first commit

---
+++ src/main/java/com/rebecamontag/projectuniversity/ProjectuniversityApplication.java
@@ -0,0 +1,13 @@
+package com.rebecamontag.projectuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectuniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectuniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebecamontag/projectuniversity/ProjectuniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebecamontag.projectuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectuniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
