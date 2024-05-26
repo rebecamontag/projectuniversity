bf11ed466a21945bc00f618b9426c0ac22db217e
Rebeca Montagnana <montagrebeca@gmail.com>
2024-05-26 17:26:22 -0300

    update and findAll methods implementation

--- src/main/java/com/rebecamontag/projectuniversity/service/ProfessorService.java
+++ src/main/java/com/rebecamontag/projectuniversity/service/ProfessorService.java
@@ -4,6 +4,8 @@ import com.rebecamontag.projectuniversity.entity.Professor;
 import com.rebecamontag.projectuniversity.repository.ProfessorRepository;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,6 +16,10 @@ public class ProfessorService {
     @Autowired
     private ProfessorRepository professorRepository;
 
+
+    public ProfessorService(ProfessorRepository professorRepository) {
+        this.professorRepository = professorRepository;
+    }
 
     public Professor create(Professor professor) {
         return professorRepository.save(professor);
@@ -29,7 +35,26 @@ public class ProfessorService {
         return professor.orElseThrow(() -> new BadRequestException("Não foi possível encontrar o professor chamado " + name));
     }
 
-    public void delete(Integer id) {
+    public Page<Professor> findAll(Pageable pageable) {
+//        PageRequest pageRequest = PageRequest.of(page, size);
+//        Page<Professor> allProfessors = professorRepository.findAll(pageRequest);
+
+        return professorRepository.findAll(pageable);
+    }
+
+    public void update(Integer id, Professor professor) {
+        Professor updatedProfessor = professorRepository.findById(id).orElseThrow();
+        updatedProfessor.setFirstName(professor.getFirstName());
+        updatedProfessor.setLastName(professor.getLastName());
+        updatedProfessor.setBirthDate(professor.getBirthDate());
+        updatedProfessor.setDocument(professor.getDocument());
+        updatedProfessor.setEmail(professor.getEmail());
+        updatedProfessor.setCourses(professor.getCourses());
+
+        professorRepository.save(updatedProfessor);
+    }
+
+    public void deleteById(Integer id) {
         professorRepository.deleteById(id);
     }
 }
