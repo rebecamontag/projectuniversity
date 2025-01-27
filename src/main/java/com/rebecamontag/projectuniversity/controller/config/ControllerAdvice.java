bafa06e1453bc0d7f1d4b6a5ec665465d80fe594
Rebeca Montagnana <montagrebeca@gmail.com>
2025-01-27 09:32:07 -0300

    fix ControllerAdvice and convert ErrorResponse into Record

--- src/main/java/com/rebecamontag/projectuniversity/controller/config/ControllerAdvice.java
+++ src/main/java/com/rebecamontag/projectuniversity/controller/config/ControllerAdvice.java
@@ -10,34 +10,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.Instant;
+
 @RestControllerAdvice
 public class ControllerAdvice {
 
     private static final Logger logger = LoggerFactory.getLogger(ControllerAdvice.class);
 
-    @ExceptionHandler
+    @ExceptionHandler(DuplicateException.class)
     public ResponseEntity<ErrorResponse> handleDuplicateException(DuplicateException duplicateException) {
         HttpStatus httpStatus = HttpStatus.CONFLICT;
 
         logger.error("DuplicateException captured: Status = {}, Error = {}, Message = {}", httpStatus, httpStatus.getReasonPhrase(), duplicateException.getMessage());
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(httpStatus.value())
-                .error(httpStatus.getReasonPhrase())
-                .message(duplicateException.getMessage())
-                .build();
+        ErrorResponse errorResponse = new ErrorResponse(Instant.now(),
+                httpStatus.value(),
+                duplicateException.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException notFoundException) {
         HttpStatus httpStatus = HttpStatus.NOT_FOUND;
 
         logger.error("DuplicateException captured: Status = {}, Error = {}, Message = {}", httpStatus, httpStatus.getReasonPhrase(), notFoundException.getMessage());
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(HttpStatus.NOT_FOUND.value())
-                .error(HttpStatus.NOT_FOUND.getReasonPhrase())
-                .message(notFoundException.getMessage())
-                .build();
+        ErrorResponse errorResponse = new ErrorResponse(Instant.now(),
+                httpStatus.value(),
+                notFoundException.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
@@ -46,11 +44,9 @@ public class ControllerAdvice {
         HttpStatus httpStatus = HttpStatus.NOT_FOUND;
 
         logger.error("DuplicateException captured: Status = {}, Error = {}, Message = {}", httpStatus, httpStatus.getReasonPhrase(), exception.getMessage());
-        ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
-                .message("Ocorreu um erro inesperado.")
-                .build();
+        ErrorResponse errorResponse = new ErrorResponse(Instant.now(),
+                httpStatus.value(),
+                "Ocorreu um erro inesperado.");
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/rebecamontag/projectuniversity/exception/ErrorResponse.java
+++ src/main/java/com/rebecamontag/projectuniversity/exception/ErrorResponse.java
@@ -1,22 +1,10 @@
 package com.rebecamontag.projectuniversity.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import java.time.Instant;
 
-import java.time.LocalDateTime;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Builder
-public class ErrorResponse {
-
-    private LocalDateTime timestamp;
-    private int status;
-    private String error;
-    private String message;
-    private String path;
-
+public record ErrorResponse(
+        Instant timestamp,
+        int status,
+        String message
+) {
 }
